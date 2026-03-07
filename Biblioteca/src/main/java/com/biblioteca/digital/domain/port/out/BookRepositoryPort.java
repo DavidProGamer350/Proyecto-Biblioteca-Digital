3136ebffeccf76a23dc42006ff99c628d7dcbcef
DAVID <DAVID@DESKTOP-1NJQIMC>
2026-03-07 10:54:05 -0500

    Actualizacion de metodo update y documento #3

--- Biblioteca/src/main/java/com/biblioteca/digital/domain/port/out/BookRepositoryPort.java
+++ Biblioteca/src/main/java/com/biblioteca/digital/domain/port/out/BookRepositoryPort.java
@@ -2,6 +2,7 @@ package com.biblioteca.digital.domain.port.out;
 
 import com.biblioteca.digital.domain.model.Book;
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepositoryPort {
     Book save(Book book);
@@ -9,4 +10,6 @@ public interface BookRepositoryPort {
     Book findByIsbn(String isbn);
     Book update(Long id, Book book);
     void deleteById(Long id);
+    Book findById(Long id);
+
 }
