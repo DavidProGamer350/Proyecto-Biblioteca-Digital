c99c473f2a2fc5272ae63ff095c41904077fe984
DavidProGamer350 <david3508675387@gmail.com>
2026-04-23 13:59:49 -0500

    Frontend modificaciones de gestion de usuarios

---
+++ Biblioteca/frontend/src/components/ProximamentePage.jsx
@@ -0,0 +1,10 @@
+export const ProximamentePage = ({ titulo }) => {
+  return (
+    <div className="container">
+      <h1>{titulo}</h1>
+      <div className="proximamente">
+        <p>Próximamente: Esta funcionalidad estará disponible pronto.</p>
+      </div>
+    </div>
+  );
+};
